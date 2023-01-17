3e0f7e540ba9971df519fe50090538c8c2e1b62a
loragaloyan <lkgaloyan@gmail.com>
2023-01-17 10:41:13 -0800

    homepage updates

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -9,12 +9,13 @@ import './common/styles/sitetypography.css';
 
 import imgBubble from './images/bubble.png';
 import imgGlobe from './images/globe.png';
-import imgMobile from './images/bubmobile.png';
+import imgMobile from './images/mobilepic.png';
 import imgVaca from './images/Portfolio/pp_vaca.png';
 import imgCode from './images/Portfolio/pp_code.png';
 import imgModel from './images/Portfolio/pp_modeling2.png';
 import imgESO from './images/Portfolio/pp_eso2.png';
 import imgArrows from './images/Portfolio/arrows.png';
+import imgPic from './images/pic.png';
 
 
 function debounce(fn, ms) {
@@ -56,19 +57,19 @@ function Portfolio() {
   function Top() {
     if (window.innerWidth < 1025 ) {
       return (
-        <div className='center-align mobile bottom-medium'>
+        <div className='center-align top-medium bottom-small'>
           <img className='bub-mobile' src= {imgMobile} width="100%"></img>
+            <h1 className='h1-primary top-medium center-align'>Hi, I'm Lora!</h1>
+            <h3 className='h2-primary center-align'>Work</h3>
+            <p className='p-primary center-align'>UX/UI & Visual Designer, Developer, 3D Modeler</p>
+            <h3 className='h2-primary center-align'>Play</h3>
+            <p className='p-primary center-align'>Author, Bookworm, Gamer, Digital Artist</p>
         </div>
       );
     }
     return (
-      <div className='flex1 box portfolio-globe-bubble'>
-        <div className='portfolio-globe'>
-          <img className='img-globe padding-medium' src= {imgGlobe} width="100%"></img>
-        </div>
-        <div className='portfolio-bubble'>
-          <img className='img-profile padding-medium' src= {imgBubble} width="100%"></img>
-        </div>
+      <div>
+        <img className='img-pic padding-medium' src= {imgPic} width="100%"></img>
       </div>
     );
   }
@@ -131,20 +132,8 @@ function Portfolio() {
     <div className='mobile'>
     <Navigation />
       <link rel="stylesheet" href="https://use.typekit.net/sxc8zwt.css"></link>
-      <div className='flexbox mobile'>
+      <div className='center-align mobile'>
         <Top />
-        <div className='flex1 box top-small'>
-          <h1 className='h1-primary mobile-center mobile-margins-2'>Hi, I'm Lora!</h1>
-          <div className='flexbox top-small'>
-            <div className='flex2'><p className='p-primary mobile-center mobile-margins-2'>UX/UI Designer, Visual Designer, Developer</p></div>
-            <div className='flex1'></div>
-          </div>
-          <div className='flexbox'>
-            <div className='flex2'><p className='p-primary mobile-center mobile-margins-2'>Digital Artist,  3D Modeler, Gamer, Bookworm, Writer</p></div>
-            <div className='flex1'></div>
-            <div className='flex1'></div>
-         </div>
-        </div>   
       </div>     
       
       <div className='center-align'>
